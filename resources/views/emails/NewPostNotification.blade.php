7a72398a01423f54d6bc85a3eadc79946c64de5b
Xavier Au <xavier.au@anacreation.com>
2016-05-12 18:14:02 +0800

    try to get path correct

--- resources/views/emails/NewPostNotification.blade.php
+++ resources/views/emails/NewPostNotification.blade.php
@@ -4,8 +4,9 @@
     <p>{{$feed->content}}</p>
     @if(count($feed->media)>0)
         @foreach($feed->media as $image)
-            <img src="{{asset($image->link)}}" alt="" style="margin:15px; max-height:200px; max-width:200px">
+            <img src="{{host $image->link)}}" alt="" style="margin:15px; max-height:200px; max-width:200px">
             <p>{{asset($image->link)}}</p>
+            <p>{{url($image->link)}}</p>
         @endforeach
     @endif
 </html>
