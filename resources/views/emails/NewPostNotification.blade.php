1f677d7fa72c1848aef13e3b150ce912d955fcdc
Xavier Au <xavier.au@anacreation.com>
2016-05-12 18:19:19 +0800

    correct email template format

--- resources/views/emails/NewPostNotification.blade.php
+++ resources/views/emails/NewPostNotification.blade.php
@@ -4,7 +4,7 @@
     <p>{{$feed->content}}</p>
     @if(count($feed->media)>0)
         @foreach($feed->media as $image)
-            <img src="{{host $image->link)}}" alt="" style="margin:15px; max-height:200px; max-width:200px">
+            <img src="{{$image->link)}}" alt="" style="margin:15px; max-height:200px; max-width:200px">
             <p>{{asset($image->link)}}</p>
             <p>{{url($image->link)}}</p>
         @endforeach
