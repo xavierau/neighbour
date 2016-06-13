e0af9145b36c3bd1d636402bc3440b7e420cd38d
Xavier Au <xavier.au@anacreation.com>
2016-06-13 15:13:49 +0800

    update bach again 1

--- app/FacebookFeed.php
+++ app/FacebookFeed.php
@@ -2,12 +2,16 @@
 
 namespace App;
 
+use App\RelationshipTraits\HasMedia;
+use App\RelationshipTraits\InStream;
 use Illuminate\Database\Eloquent\Model;
 
 class FacebookFeed extends Model
 {
+    use HasMedia, InStream;
+
     protected $fillable = [
-        "message", "message_id", "author_id", "reply_to"
+        "message", "message_id", "author_id", "reply_to", "created_at"
     ];
 
 }
--- app/Http/Controllers/FacebookFeedsController.php
+++ app/Http/Controllers/FacebookFeedsController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 
 use App\FacebookFeed;
-use App\User;
-use Facebook\Facebook;
+use App\FacebookUser;
 use App\Http\Requests;
+use Carbon\Carbon;
+use Facebook\Facebook;
 use Illuminate\Support\Facades\DB;
 
 class FacebookFeedsController extends Controller
@@ -14,16 +15,21 @@ class FacebookFeedsController extends Controller
     public function create()
     {
         $fb = new Facebook([
-            'app_id' => env("FACEBOOK_APP_ID"),
-            'app_secret' => env("FACEBOOK_APP_SECRET"),
+            'app_id'                => env("FACEBOOK_APP_ID"),
+            'app_secret'            => env("FACEBOOK_APP_SECRET"),
             'default_graph_version' => 'v2.6',
         ]);
         $token = DB::table("facebook_users")->first()->token;
         $fields = [
-          'limit'=>5,
-            'format'=>'json',
-            'access_token'=>$token,
-            'fields'=>"from, comments, message, type"
+            'limit'        => 5,
+            'format'       => 'json',
+            'access_token' => $token,
+            'fields'       => "
+            from{id, name, picture}, 
+            comments{from{id, name, picture} ,id, type, message, created_time}, 
+            message, 
+            type,
+            created_time"
         ];
         $query = http_build_query($fields);
         $facebookPageId = env("FACEBOOK_PAGE_ID");
@@ -33,46 +39,128 @@ class FacebookFeedsController extends Controller
         $feedEdge = $response->getGraphEdge();
 
         $this->getFeeds($fb, $feedEdge);
+
         return redirect('/');
     }
 
-    private function createRecords($item){
+    private function createRecords($item)
+    {
         $object = json_decode($item);
-        if(isset($object->message)) {
-            $newFeed = FacebookFeed::create([
-                'message'=>$object->message,
-                "message_id"=>$object->id,
-                "reply_to"=>0,
-                "author_id"=>$object->from->id
-            ]);
+        if ($object->type == 'status') {
+            $newFbFeed = $this->createFbFeedRecord($object);
 
-            if(isset($object->comments)) {
-                array_map(function($comment)use($newFeed){
-                    FacebookFeed::create([
-                        'message'=>$comment->message,
-                        "message_id"=>$comment->id,
-                        "reply_to"=>$newFeed->id,
-                        "author_id"=>$comment->from->id
-                    ]);
-                }, $object->comments);
-
+            if (isset($object->comments)) {
+                foreach ($object->comments as $comment){
+                    $this->createFbFeedRecord($comment, $newFbFeed);
+                }
             }
-
         }
+    }
 
+    private function fbUserExist($fdUserId):bool
+    {
+        $user = FacebookUser::whereId($fdUserId)->first();
+
+        return !!$user;
     }
 
     private function getFeeds($fb, $edge)
     {
-        
+
         foreach ($edge as $item) {
-            logger($item);
             $this->createRecords($item);
         }
-        $nextBatch = null;
+
         $nextBatch = $fb->next($edge);
         if ($nextBatch) {
             $this->getFeeds($fb, $nextBatch);
         }
+    }
+
+    /**
+     * @param $fbUserEdge
+     */
+    private function createFbUser($fbUserEdge)
+    {
+        $data = [
+            "id"   => $fbUserEdge->id,
+            "name" => $fbUserEdge->name,
+        ];
+
+        if (isset($fbUserEdge->email)) {
+            $data["email"] = $fbUserEdge->email;
+        }
+        if (isset($fbUserEdge->picture->url)) {
+            $data["avatar"] = $fbUserEdge->picture->url;
+        }
+
+        FacebookUser::create($data);
+    }
+
+    /**
+     * @param                   $fbFeed
+     * @param \App\FacebookFeed $replyFeed
+     * @return static
+     */
+    private function createFbFeedRecord($fbFeed, FacebookFeed $replyFeed=null)
+    {
+        if(! $this->alreadyExist($fbFeed)){
+
+            $data = [
+                'message'    => $fbFeed->message,
+                "message_id" => $fbFeed->id,
+                "reply_to"   => $replyFeed?$replyFeed->id:0,
+                "author_id"  => $fbFeed->from->id,
+            ];
+
+            if(isset($fbFeed->created_time)){
+                if(is_string($fbFeed->created_time)){
+                    $data['created_at'] = $this->covertFacebookTimeStamp($fbFeed->created_time);
+                }else{
+                    $data['created_at'] = $this->covertFacebookTimeStamp($fbFeed->created_time->date);
+                }
+            }
+
+            $newFeed = FacebookFeed::create($data);
+
+            if(isset($fbFeed->picture)){
+                $data = [
+                    'link' => $fbFeed->picture,
+                    'type' => 'image'
+                ];
+                $newFeed->media()->media()->create([])->update($data);;
+            }
+
+            if(!$this->fbUserExist($fbFeed->from->id)){
+                $this->createFbUser($fbFeed->from);
+            }
+
+            if(!$replyFeed){
+                $this->addToStream($newFeed);
+            }
+
+            return $newFeed;
+        }
+    }
+
+    private function alreadyExist($fbFeed):bool
+    {
+        $feed = FacebookFeed::whereMessageId($fbFeed->id)->first();
+
+        return !! $feed;
+    }
+
+    private function addToStream($newFeed)
+    {
+        $stream =$newFeed->stream()->create([]);
+        $stream->item_id = $newFeed->id;
+        $stream->created_at = $newFeed->created_at;
+        $stream->save();
+    }
+
+    private function covertFacebookTimeStamp($created_time)
+    {
+        $dateTimeObject = new Carbon($created_time);
+        return $dateTimeObject->addHours(8)->toDateTimeString();
     }
 }
