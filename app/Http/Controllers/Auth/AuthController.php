347e48f4274b4026ef08ebd066f060f6cf693e5a
Xavier Au <xavier.au@anacreation.com>
2016-09-11 07:53:37 +0800

    update again .ENV sample

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,21 +4,18 @@ namespace App\Http\Controllers\Auth;
 
 use App\City;
 use App\Clan;
-use App\FacebookUser;
+use App\Http\Controllers\Controller;
 use App\Role;
 use App\Services\FbServices;
 use App\User;
 use App\UserStatus;
 use App\UserType;
-use Facebook\FacebookRequest;
-use GuzzleHttp\Client;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Validator;
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
@@ -48,53 +45,50 @@ class AuthController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
     }
 
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
-    {
+    protected function validator(array $data) {
         $pattern = "/((?:[0-9]{1,})(?:[a-zA-Z]{1,}))|((?:[a-zA-Z]{1,})(?:[0-9]{1,}))/";
 
         return Validator::make($data, [
             'first_name' => 'required|max:255',
-            'last_name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => ["required", "min:5", "confirmed", "regex:".$pattern],
-            'clan_id' => 'required|in:'.implode(",", Clan::lists('id')->toArray()),
-            'city_id' => 'required|in:'.implode(",", City::lists('id')->toArray()),
+            'last_name'  => 'required|max:255',
+            'email'      => 'required|email|max:255|unique:users',
+            'password'   => ["required", "min:5", "confirmed", "regex:" . $pattern],
+            'clan_id'    => 'required|in:' . implode(",", Clan::lists('id')->toArray()),
+            'city_id'    => 'required|in:' . implode(",", City::lists('id')->toArray()),
         ]);
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return User
      */
-    protected function create(array $data)
-    {
+    protected function create(array $data) {
         $role = Role::whereCode("gen")->first();
         $userStatus = UserStatus::whereCode('pending')->first();
         $userType = UserType::whereType('app')->first();
 
         $user = $userStatus->users()->create([
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'city_id' => $data['city_id'],
-            'clan_id' => $data['clan_id'],
-            'user_type_id' => $userType->id,
-            'email' => $data['email'],
-            'password' => $data['password'],
-            'status' =>'pending',
-        ]);
+                                                 'first_name'   => $data['first_name'],
+                                                 'last_name'    => $data['last_name'],
+                                                 'city_id'      => $data['city_id'],
+                                                 'clan_id'      => $data['clan_id'],
+                                                 'user_type_id' => $userType->id,
+                                                 'email'        => $data['email'],
+                                                 'password'     => $data['password'],
+                                                 'status'       => 'pending',
+                                             ]);
 
         $user->roles()->save($role);
 
@@ -104,17 +98,14 @@ class AuthController extends Controller
     /**
      * Handle a registration request for the application.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
-    {
+    public function register(Request $request) {
         $validator = $this->validator($request->all());
 
         if ($validator->fails()) {
-            $this->throwValidationException(
-                $request, $validator
-            );
+            $this->throwValidationException($request, $validator);
         }
 
         $this->create($request->all());
@@ -126,11 +117,10 @@ class AuthController extends Controller
     /**
      * Handle a login request to the application.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function login(Request $request)
-    {
+    public function login(Request $request) {
         $this->validateLogin($request);
 
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
@@ -156,55 +146,57 @@ class AuthController extends Controller
         // If the login attempt was unsuccessful we will increment the number of attempts
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
-        if ($throttles && ! $lockedOut) {
+        if ($throttles && !$lockedOut) {
             $this->incrementLoginAttempts($request);
         }
 
         return $this->sendFailedLoginResponse($request);
     }
 
-    public function facebookSignUp()
-    {
+    public function facebookSignUp() {
         return Socialite::driver('facebook')->scopes([
-            'user_managed_groups', 'email','user_status','user_relationships','user_friends'
-        ])->redirect();
+                                                         'user_managed_groups',
+                                                         'email',
+                                                         'user_status',
+                                                         'user_relationships',
+                                                         'user_friends'
+                                                     ])->redirect();
     }
 
-    public function facebookLogin()
-    {
+    public function facebookLogin() {
         return Socialite::driver('facebook')->scopes([
-            'user_managed_groups', 'email','user_status','user_relationships','user_friends'
-        ])->redirect();
+                                                         'user_managed_groups',
+                                                         'email',
+                                                         'user_status',
+                                                         'user_relationships',
+                                                         'user_friends'
+                                                     ])->redirect();
     }
 
-    public function handleFacebookSignUpCallback()
-    {
+    public function handleFacebookSignUpCallback() {
         $fbUser = Socialite::driver('facebook')->user();
 
-        $service  = new FbServices($fbUser);
+        $service = new FbServices($fbUser);
 
         $user = $service->fetchOrCreateAppUserFromFacebookUserGraph();
 
         $service->fetchFeedFromGroup();
 
         Auth::login($user);
-            return redirect('/');
 
-
-
+        return redirect('/');
     }
 
-    public function twitterSignUp()
-    {
+    public function twitterSignUp() {
         return Socialite::driver('twitter')->redirect();
     }
 
-    public function handleTwitterSignUpCallback()
-    {
+    public function handleTwitterSignUpCallback() {
         try {
             $user = Socialite::driver('twitter')->user();
         } catch (Exception $e) {
             dd($e);
+
             return redirect('auth/twitter');
         }
 
@@ -215,72 +207,66 @@ class AuthController extends Controller
         return redirect()->route('home');
     }
 
-    private function findOrCreateUser($twitterUser)
-    {
+    private function findOrCreateUser($twitterUser) {
         $userTypeConst = "TWITTER";
         $authUser = User::where('twitter_id', $twitterUser->id)->first();
-        if ($authUser){
+        if ($authUser) {
             return $authUser;
         }
-        $type = constant("\App\Enums\UserType::".$userTypeConst);
+        $type = constant("\App\Enums\UserType::" . $userTypeConst);
         $userType = UserType::whereType($type)->first();
 
         $data = [
-            'name' => $twitterUser->name,
-            'handle' => $twitterUser->nickname,
+            'name'       => $twitterUser->name,
+            'handle'     => $twitterUser->nickname,
             'twitter_id' => $twitterUser->id,
-            'avatar' => $twitterUser->avatar_original
+            'avatar'     => $twitterUser->avatar_original
         ];
 
         $userType->users()->create($data);
 
         return User::create([
-            'name' => $twitterUser->name,
-            'handle' => $twitterUser->nickname,
-            'twitter_id' => $twitterUser->id,
-            'avatar' => $twitterUser->avatar_original
-        ]);
+                                'name'       => $twitterUser->name,
+                                'handle'     => $twitterUser->nickname,
+                                'twitter_id' => $twitterUser->id,
+                                'avatar'     => $twitterUser->avatar_original
+                            ]);
     }
 
     /**
      * Create the response for when a request fails validation.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  array  $errors
+     * @param  \Illuminate\Http\Request $request
+     * @param  array                    $errors
      * @return \Illuminate\Http\Response
      */
-    protected function buildFailedValidationResponse(Request $request, array $errors)
-    {
-        if (($request->ajax() && ! $request->pjax()) || $request->wantsJson()) {
+    protected function buildFailedValidationResponse(Request $request, array $errors) {
+        if (($request->ajax() && !$request->pjax()) || $request->wantsJson()) {
             return new JsonResponse($errors, 422);
         }
 
-        return redirect()->to($this->getRedirectUrl())
-            ->withInput($request->input())
-            ->withFrom($request->path())
-            ->withErrors($errors, $this->errorBag());
+        return redirect()->to($this->getRedirectUrl())->withInput($request->input())->withFrom($request->path())->withErrors($errors,
+                                                                                                                             $this->errorBag());
     }
 
     /**
      * Get the failed login response instance.
      *
-     * @param \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    protected function sendFailedLoginResponse(Request $request)
-    {
-        return redirect()->back()
-            ->withInput($request->only($this->loginUsername(), 'remember'))
-            ->withFrom($request->path())
-            ->withErrors([
-                $this->loginUsername() => $this->getFailedLoginMessage(),
-            ]);
+    protected function sendFailedLoginResponse(Request $request) {
+        return redirect()->back()->withInput($request->only($this->loginUsername(),
+                                                            'remember'))->withFrom($request->path())->withErrors([
+                                                                                                                     $this->loginUsername() => $this->getFailedLoginMessage(),
+                                                                                                                 ]);
     }
 
     protected function authenticated(Request $request, User $authUser) {
 
-        if($authUser->is('sadmin'))
+        if ($authUser->is('sadmin')) {
             return redirect('/admin/dashboard');
+        }
 
         return redirect()->intended($this->redirectPath());
 
